db49c350cb0af8750dea9c920f141106482f6bf4
ele <eselectron@163.com>
2023-11-17 22:22:30 +0800

    Use Next UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Accordion, AccordionItem, Link} from "@nextui-org/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Accordion>
+      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
+        <li>
+          <Link href="#">Default Link</Link>
+        </li>
+        <li>
+          <Link href="#">Default Link</Link>
+        </li>
+        <li>
+          <Link href="#">Default Link</Link>
+        </li>
+      </AccordionItem>
+      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
+        <Link href="#">Default Link</Link>
+      </AccordionItem>
+      <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
+        <Link href="#">Default Link</Link>
+      </AccordionItem>
+    </Accordion>
   );
 }
 
